8f903c7c02b56663bf2ce65ea1ba62048929fc34
João Lucas Carvalho <88744662+Joaolpridolficarvalho@users.noreply.github.com>
2023-01-31 15:20:06 -0300

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-import main
-
-date_initial = "13/12/2022"
-date_final = "14/12/2022"
-d1 = main.Dolar(date_initial, date_final)
-value = d1.show_values()
-conversion = d1.convert_Reais_in_dollars(30.90)
-
